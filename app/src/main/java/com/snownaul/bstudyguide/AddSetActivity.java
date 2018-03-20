8a29ca4f9ce2470ac1f3e191b13ea60d22f08b00
SnowNaul <doolri1277@gmail.com>
2018-03-20 19:21:08 +0900

    이제 세트 추가하는 버튼 누르면 나오는 화면 만들거다..

---
+++ app/src/main/java/com/snownaul/bstudyguide/AddSetActivity.java
@@ -0,0 +1,13 @@
+package com.snownaul.bstudyguide;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddSetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_set);
+    }
+}
--- app/src/main/java/com/snownaul/bstudyguide/Page1Fragment.java
+++ app/src/main/java/com/snownaul/bstudyguide/Page1Fragment.java
@@ -1,8 +1,10 @@
 package com.snownaul.bstudyguide;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,11 +15,25 @@ import android.view.ViewGroup;
 
 public class Page1Fragment extends Fragment {
 
+    CardView btnAddSet;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.page1,container,false);
 
+        btnAddSet=view.findViewById(R.id.btn_addset);
+        btnAddSet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getContext(),AddSetActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
         return view;
     }
+
+
 }
